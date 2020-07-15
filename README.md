d2637df1354d6e445b1c6c9b50aea54f6bffce36
RKSI <56246212+Rama3214@users.noreply.github.com>
2020-07-16 02:18:58 +0800

    Update and rename README.md to python bassoon.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# fuzzy-octo-bassoon
-Hacker max
---
+++ python bassoon.py
@@ -0,0 +1,37 @@
+# fuzzy-octo-bassoon
+Peretas maks
+apt update && apt upgrade
+apt install git
+apt install php
+apt install curl
+apt install ruby
+apt install figlet
+apt install python2
+gem install lolcat
+pkg install git
+cd fuzzy-octo-bassoon
+python fuzzy-octo-bassoon.py
+@@ -1,13 +1,13 @@
+# Crack-Elite
+<ul>
+<li><code>$ apt update && apt upgrade</code></li>
+<li><code>$ pkg install python2</code></li>
+<li><code>$ pip2 install mechanize</code></li>
+<li><code>$ pip2 install requests</code></li>
+<li><code>$ pkg install git</code></li>
+<li><code>$ git clone https:///git clone https://github.com/Rama3214/fuzzy-octo-bassoon
+</code></li>
+<li><code>$ cd fuzzy-octo-bassoon</code></li>
+<li><code>$ cd fuzzy-octo-bassoon
+</code></li>
+<li><code>$ ls</code></li>
+<li><code>$ python2 fuzzy-octo-bassoon.py</code></li>
+<ul>
+<li><code> Silahkan download lewat Uc Mini/Uc Browser! </code></li>
+<li><code> Username & Pasword  : https://hack </code></li>
+<li><code> Nih biar ga kena Cp : https://max</code></li>
+</ul>
+<br/>
+0 comments on commit 028405c
+Write
+Preview
